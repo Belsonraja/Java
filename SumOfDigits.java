ef5b7f59c88fb01522e8001f782ff009f639bb43
BELSONRAJA T <32982789+Belsonraja@users.noreply.github.com>
2019-02-27 19:48:35 +0530

    Updated for tuning code
    Updated for tuning code

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,14 +1,14 @@
 public class SumOfDigits {
 	public static void main(String[] args) {
 		int a = 123456,b=0; /*Sum of integer into single digit*/
-		while(a>9){
-			b=0;
-			while(a>0){
-				b += a%10;
-				a /=10;
+		while(a>0){
+			b += a%10;
+			a /= 10;
+			if(a == 0 && b > 9){
+				a = b;
+				b = 0;
 			}
-			a=b;
 		}
-		System.out.println("Output : "+a);
+		System.out.println("Output : "+b);
 	}
 }
